a4b1c0a3fc41d19b65213aa285d57dd4727b2bb1
stefan <sa.stefan.andjelkovic@gmail.com>
2017-02-25 10:10:02 +0100

    Switch to Project Lombok for Trakt DTOs

--- src/main/java/com/sandjelkovic/dispatchd/trakt/dto/EpisodeTrakt.java
+++ src/main/java/com/sandjelkovic/dispatchd/trakt/dto/EpisodeTrakt.java
@@ -2,11 +2,17 @@ package com.sandjelkovic.dispatchd.trakt.dto;
 
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
 
+@Data
+@RequiredArgsConstructor
+@AllArgsConstructor
 public class EpisodeTrakt {
 	private String season;
 	private Integer number;
@@ -18,73 +24,4 @@ public class EpisodeTrakt {
 	@JsonProperty("first_aired")
 	private Instant firstAired;
 
-	public String getSeason() {
-		return season;
-	}
-
-	public Integer getNumber() {
-		return number;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public Map<String, String> getIds() {
-		return ids;
-	}
-
-	public String getOverview() {
-		return overview;
-	}
-
-	public Instant getUpdatedAt() {
-		return updatedAt;
-	}
-
-	public Instant getFirstAired() {
-		return firstAired;
-	}
-
-	@Override
-	public String toString() {
-		return "EpisodeTrakt{" +
-				"season='" + season + '\'' +
-				", number=" + number +
-				", title='" + title + '\'' +
-				", ids=" + ids +
-				", overview='" + overview + '\'' +
-				", updatedAt=" + updatedAt +
-				", firstAired=" + firstAired +
-				'}';
-	}
-
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) return true;
-		if (o == null || getClass() != o.getClass()) return false;
-
-		EpisodeTrakt that = (EpisodeTrakt) o;
-
-		if (season != null ? !season.equals(that.season) : that.season != null) return false;
-		if (number != null ? !number.equals(that.number) : that.number != null) return false;
-		if (title != null ? !title.equals(that.title) : that.title != null) return false;
-		if (ids != null ? !ids.equals(that.ids) : that.ids != null) return false;
-		if (overview != null ? !overview.equals(that.overview) : that.overview != null) return false;
-		if (updatedAt != null ? !updatedAt.equals(that.updatedAt) : that.updatedAt != null) return false;
-		return firstAired != null ? firstAired.equals(that.firstAired) : that.firstAired == null;
-
-	}
-
-	@Override
-	public int hashCode() {
-		int result = season != null ? season.hashCode() : 0;
-		result = 31 * result + (number != null ? number.hashCode() : 0);
-		result = 31 * result + (title != null ? title.hashCode() : 0);
-		result = 31 * result + (ids != null ? ids.hashCode() : 0);
-		result = 31 * result + (overview != null ? overview.hashCode() : 0);
-		result = 31 * result + (updatedAt != null ? updatedAt.hashCode() : 0);
-		result = 31 * result + (firstAired != null ? firstAired.hashCode() : 0);
-		return result;
-	}
 }
