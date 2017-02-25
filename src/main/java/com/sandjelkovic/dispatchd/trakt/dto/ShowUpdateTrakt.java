bd94b820a6b099789011424b240c9a6303981046
stefan <sa.stefan.andjelkovic@gmail.com>
2017-02-25 10:10:02 +0100

    Switch to Project Lombok for Trakt DTOs

--- src/main/java/com/sandjelkovic/dispatchd/trakt/dto/ShowUpdateTrakt.java
+++ src/main/java/com/sandjelkovic/dispatchd/trakt/dto/ShowUpdateTrakt.java
@@ -1,6 +1,9 @@
 package com.sandjelkovic.dispatchd.trakt.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 import java.time.ZonedDateTime;
 
@@ -8,42 +11,11 @@ import java.time.ZonedDateTime;
  * @author ${sandjelkovic}
  * @date 5.2.17.
  */
+@Data
+@RequiredArgsConstructor
+@AllArgsConstructor
 public class ShowUpdateTrakt {
 	@JsonProperty("updated_at")
 	private ZonedDateTime updatedAt;
 	private TvShowTrakt show;
-
-	public ShowUpdateTrakt updatedAt(final ZonedDateTime updatedAt) {
-		this.updatedAt = updatedAt;
-		return this;
-	}
-
-	public ShowUpdateTrakt show(final TvShowTrakt show) {
-		this.show = show;
-		return this;
-	}
-
-	public ZonedDateTime getUpdatedAt() {
-		return updatedAt;
-	}
-
-	public void setUpdatedAt(ZonedDateTime updatedAt) {
-		this.updatedAt = updatedAt;
-	}
-
-	public TvShowTrakt getShow() {
-		return show;
-	}
-
-	public void setShow(TvShowTrakt show) {
-		this.show = show;
-	}
-
-	@Override
-	public String toString() {
-		return "ShowUpdateTrakt{" +
-				"updatedAt=" + updatedAt +
-				", show=" + show +
-				'}';
-	}
 }
--- src/main/java/com/sandjelkovic/dispatchd/trakt/dto/TvShowTrakt.java
+++ src/main/java/com/sandjelkovic/dispatchd/trakt/dto/TvShowTrakt.java
@@ -2,12 +2,18 @@ package com.sandjelkovic.dispatchd.trakt.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
+@Data
+@RequiredArgsConstructor
+@AllArgsConstructor
 public class TvShowTrakt {
 	private String title;
 	private Integer year;
