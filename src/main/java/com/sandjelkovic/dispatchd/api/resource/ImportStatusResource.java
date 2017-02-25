0816a2df6e6a837303306944c5786b28d6708101
stefan <sa.stefan.andjelkovic@gmail.com>
2017-02-25 11:50:50 +0100

    Change Link Logic in ImportStatusResource

--- src/main/java/com/sandjelkovic/dispatchd/api/resource/ImportStatusResource.java
+++ src/main/java/com/sandjelkovic/dispatchd/api/resource/ImportStatusResource.java
@@ -3,6 +3,7 @@ package com.sandjelkovic.dispatchd.api.resource;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.sandjelkovic.dispatchd.api.controller.ImportRestController;
 import com.sandjelkovic.dispatchd.api.dto.ImportStatusDto;
+import com.sandjelkovic.dispatchd.api.link.RelNamesConstants;
 import org.springframework.hateoas.ResourceSupport;
 import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 
@@ -15,7 +16,8 @@ public class ImportStatusResource extends ResourceSupport {
 
 	public ImportStatusResource(ImportStatusDto data) {
 		this.data = data;
-		this.add(ControllerLinkBuilder.linkTo(methodOn(ImportRestController.class).getImportStatus(data.getId())).withSelfRel());
+		this.add(ControllerLinkBuilder.linkTo(methodOn(ImportRestController.class).doImport(null)).withSelfRel());
+		this.add(ControllerLinkBuilder.linkTo(methodOn(ImportRestController.class).getImportStatus(data.getId())).withRel(RelNamesConstants.IMPORT_STATUS));
 	}
 
 	public ImportStatusDto getData() {
