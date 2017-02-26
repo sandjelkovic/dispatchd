7a5066b56057c07e797aa8fe5e5db442cca1c496
stefan <sa.stefan.andjelkovic@gmail.com>
2017-02-26 12:55:04 +0100

    Fix tests.

--- src/test/java/com/sandjelkovic/dispatchd/domain/data/entity/BasicEntityTest.java
+++ src/test/java/com/sandjelkovic/dispatchd/domain/data/entity/BasicEntityTest.java
@@ -3,16 +3,16 @@ package com.sandjelkovic.dispatchd.domain.data.entity;
 import com.sandjelkovic.dispatchd.DispatchdApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = DispatchdApplication.class)
-@WebAppConfiguration
+@ActiveProfiles(profiles = {"testing"})
+@SpringBootTest(classes = {DispatchdApplication.class})
 public class BasicEntityTest {
 
 	private BasicEntity currentEntity;
