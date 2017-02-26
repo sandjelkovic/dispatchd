b750ab5a3e7a550307e38651851a554e98a96abb
stefan <sa.stefan.andjelkovic@gmail.com>
2017-02-26 13:12:49 +0100

    Move Link creation logic to ResourceProcessor. Create base infrastructure for calling ResourceProcessors

---
+++ src/main/java/com/sandjelkovic/dispatchd/api/controller/BaseController.java
@@ -0,0 +1,15 @@
+package com.sandjelkovic.dispatchd.api.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.mvc.ResourceProcessorInvoker;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author ${sandjelkovic}
+ * @date 26.2.17.
+ */
+@Component
+public class BaseController {
+	@Autowired
+	protected ResourceProcessorInvoker resourceProcessorInvoker;
+}
