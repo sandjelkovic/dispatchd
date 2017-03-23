a88e02347054bb48aeb38561995756783f7898b0
stefan <sa.stefan.andjelkovic@gmail.com>
2017-03-23 21:14:19 +0100

    #28 Move ReportTemplate relation endpoints to a new Controller

--- src/main/java/com/sandjelkovic/dispatchd/api/controller/ReportTemplateController.java
+++ src/main/java/com/sandjelkovic/dispatchd/api/controller/ReportTemplateController.java
@@ -1,16 +1,11 @@
 package com.sandjelkovic.dispatchd.api.controller;
 
-import com.sandjelkovic.dispatchd.api.dto.RelationDTO;
 import com.sandjelkovic.dispatchd.api.dto.ReportTemplateDTO;
-import com.sandjelkovic.dispatchd.api.dto.ShowConnectionsDTO;
-import com.sandjelkovic.dispatchd.api.dto.TvShowDTO;
 import com.sandjelkovic.dispatchd.api.resource.ReportTemplateResource;
-import com.sandjelkovic.dispatchd.api.resource.TvShowListResource;
 import com.sandjelkovic.dispatchd.api.resource.UserReportTemplateListResource;
 import com.sandjelkovic.dispatchd.configuration.Constants;
 import com.sandjelkovic.dispatchd.converter.ReportTemplate2DTOConverter;
 import com.sandjelkovic.dispatchd.domain.data.entity.ReportTemplate;
-import com.sandjelkovic.dispatchd.domain.data.entity.TvShow;
 import com.sandjelkovic.dispatchd.domain.data.entity.User;
 import com.sandjelkovic.dispatchd.domain.facade.ReportFacade;
 import com.sandjelkovic.dispatchd.domain.service.UserService;
@@ -32,13 +27,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 import java.security.Principal;
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.HttpStatus.OK;
-import static org.springframework.web.bind.annotation.RequestMethod.DELETE;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 import static org.springframework.web.bind.annotation.RequestMethod.PUT;
@@ -113,54 +105,6 @@ public class ReportTemplateController extends BaseController {
 
 		ReportTemplateResource reportTemplateResource = new ReportTemplateResource(returnTemplate);
 		return resourceProcessorInvoker.invokeProcessorsFor(reportTemplateResource);
-	}
-
-	@RequestMapping(value = "/{templateId}", method = DELETE)
-	@ResponseStatus(OK)
-	public void deleteTemplate(@PathVariable Long templateId) {
-		reportFacade.deleteTemplate(templateId);
-	}
-
-	@RequestMapping(value = "/{templateId}/shows", method = POST)
-	@ResponseStatus(OK)
-	public void connectWithShows(@PathVariable Long templateId, @RequestBody ShowConnectionsDTO showConnectionsDTO) {
-		// create new relations (update if existing or throw error?)
-	}
-
-	@RequestMapping(value = "/{templateId}/shows", method = PUT)
-	@ResponseStatus(OK)
-	public void updateConnectionsToShow(@PathVariable Long templateId, @RequestBody ShowConnectionsDTO showConnectionsDTO) {
-		// update  or create relations template <> shows. Update order if already present
-	}
-
-	@RequestMapping(value = "/{templateId}/shows/{showId}", method = PUT)
-	@ResponseStatus(OK)
-	public void updateConnectionToShow(@PathVariable Long templateId, @RequestBody RelationDTO relationDTO) {
-		// update relation template <> show. Update order in collection
-	}
-
-	@RequestMapping(value = "/{templateId}/shows/{showId}", method = DELETE)
-	@ResponseStatus(OK)
-	public void deleteConnectionToShow(@PathVariable Long templateId, @PathVariable String showId) {
-		// delete relations template <> shows. Update order if present
-	}
-
-	@RequestMapping(value = "/{templateId}/shows", method = DELETE)
-	@ResponseStatus(OK)
-	public void deleteConnectionsToShow(@PathVariable Long templateId, @RequestBody ShowConnectionsDTO showConnectionsDTO) {
-		// delete relations template <> shows. Update order if present
-	}
-
-	@RequestMapping(value = "/{templateId}/shows", method = GET)
-	@ResponseStatus(OK)
-	public TvShowListResource getConnectionsToShow(@PathVariable Long templateId) {
-		// retrieve all shows connected to this template
-		List<TvShow> shows = reportFacade.findTemplateShows(templateId);
-		List<TvShowDTO> showDtos = shows.stream()
-				.map(show -> conversionService.convert(show, TvShowDTO.class))
-				.collect(Collectors.toList());
-
-		return null;
 	}
 
 	private void isTemplateIsOwnedByUser(Long templateId, Principal principal) {
