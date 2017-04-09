191eede264234a472e3592cf9a5b102e36ef0b0c
stefan <sa.stefan.andjelkovic@gmail.com>
2017-04-09 13:33:58 +0200

    Add Monitor service to the repo

---
+++ monitor-service/src/main/java/com/sandjelkovic/dispatch/monitor/DispatchdMonitorApplication.java
@@ -0,0 +1,14 @@
+package com.sandjelkovic.dispatch.monitor;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAdminServer
+public class DispatchdMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DispatchdMonitorApplication.class, args);
+	}
+}
