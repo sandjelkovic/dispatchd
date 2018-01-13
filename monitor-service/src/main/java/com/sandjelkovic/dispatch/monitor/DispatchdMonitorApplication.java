849678ed738a360872ed713f700b88288584a486
stefan <sa.stefan.andjelkovic@gmail.com>
2018-01-13 16:05:25 +0100

    Disconnect Admin service until it supports Spring Boot 2. See https://github.com/codecentric/spring-boot-admin/issues/465

--- monitor-service/src/main/java/com/sandjelkovic/dispatch/monitor/DispatchdMonitorApplication.java
+++ monitor-service/src/main/java/com/sandjelkovic/dispatch/monitor/DispatchdMonitorApplication.java
@@ -1,11 +1,13 @@
 package com.sandjelkovic.dispatch.monitor;
 
-import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableAdminServer
+// Disconnect Admin server until it supports Spring Boot 2
+// See https://github.com/codecentric/spring-boot-admin/issues/465
+
+//@EnableAdminServer
 public class DispatchdMonitorApplication {
 
 	public static void main(String[] args) {
