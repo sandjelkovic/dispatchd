57b9ae1cdb6253d77c77489494c25bb3ae04f984
stefan <sa.stefan.andjelkovic@gmail.com>
2018-01-28 11:31:17 +0100

    Configure properties for Trakt remote calls

--- content-service/src/main/kotlin/com/sandjelkovic/dispatchd/contentservice/ContentConfig.kt
+++ content-service/src/main/kotlin/com/sandjelkovic/dispatchd/contentservice/ContentConfig.kt
@@ -1,14 +1,40 @@
 package com.sandjelkovic.dispatchd.contentservice
 
+import com.sandjelkovic.dispatchd.contentservice.interceptor.HeaderRequestInterceptor
+import mu.KLogging
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.cloud.context.config.annotation.RefreshScope
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.client.RestTemplate
 
 /**
  * @author sandjelkovic
  * @date 26.1.18.
  */
 @Configuration
-class ContentConfig {
-    @Value("\${content.refresh.interval.minutes:1}")
-    var refreshInterval: Int = 0
+@RefreshScope
+class ContentConfig(
+        @Value("\${content.refresh.interval.minutes:1}")
+        var refreshInterval: Int,
+        @Value("\${trakt.baseServiceUrl: }")
+        var baseServiceUrl: String,
+        @Value("\${trakt.appSecret: }")
+        var appSecret: String) {
+
+    companion object : KLogging()
+
+    @Bean
+    @RefreshScope
+    fun traktRestTemplate(
+            @Value("\${trakt.appId: }") appId: String,
+            @Value("\${trakt.apiVersion: }") apiVersion: String): RestTemplate {
+        val restTemplate = RestTemplate()
+        restTemplate.interceptors.add(HeaderRequestInterceptor("Content-type", "application/json"))
+        restTemplate.interceptors.add(HeaderRequestInterceptor("trakt-api-key", appId))
+        restTemplate.interceptors.add(HeaderRequestInterceptor("trakt-api-version", apiVersion))
+        logger.debug("Configured TraktRestTemplate with api key: ${appId} and api version: ${apiVersion}")
+        return restTemplate
+    }
+
 }
