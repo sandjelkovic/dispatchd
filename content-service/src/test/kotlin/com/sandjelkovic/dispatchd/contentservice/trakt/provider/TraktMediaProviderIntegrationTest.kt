aedc19b61d46781c67e32fde9443c7220fd04fb3
stefan <sa.stefan.andjelkovic@gmail.com>
2018-02-03 14:51:17 +0100

    Enhance test to use file based stubs

--- content-service/src/test/kotlin/com/sandjelkovic/dispatchd/contentservice/trakt/provider/TraktMediaProviderIntegrationTest.kt
+++ content-service/src/test/kotlin/com/sandjelkovic/dispatchd/contentservice/trakt/provider/TraktMediaProviderIntegrationTest.kt
@@ -1,8 +1,10 @@
 package com.sandjelkovic.dispatchd.contentservice.trakt.provider
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.sandjelkovic.dispatchd.contentservice.data.entity.Show
 import com.sandjelkovic.dispatchd.contentservice.trakt.provider.impl.DefaultTraktMediaProvider
+import org.assertj.core.api.Assertions.assertThat
+import org.hamcrest.Matchers.containsString
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
@@ -10,6 +12,7 @@ import org.springframework.boot.autoconfigure.ImportAutoConfiguration
 import org.springframework.boot.test.autoconfigure.web.client.AutoConfigureWebClient
 import org.springframework.boot.test.autoconfigure.web.client.RestClientTest
 import org.springframework.cloud.autoconfigure.RefreshAutoConfiguration
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver
 import org.springframework.http.MediaType
 import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.test.web.client.MockRestServiceServer
@@ -34,14 +37,50 @@ class TraktMediaProviderIntegrationTest {
     @Autowired
     lateinit var objectMapper: ObjectMapper
 
+    private val resourceResolver = PathMatchingResourcePatternResolver()
+
+    @Before
+    fun setUp() {
+        val stubsRoot = "/stubs"
+        loadSubResourcesRecursive("", stubsRoot)
+    }
+
+    private fun loadSubResourcesRecursive(resourcePath: String, root: String) {
+        resourceResolver
+                .getResources("${root}${resourcePath}/*")
+                .map { it.filename }
+                .forEach { resourceName ->
+                    val subResourcePath = "${resourcePath}/$resourceName"
+                    loadThis(subResourcePath, root)
+                    loadSubResourcesRecursive(subResourcePath, root)
+                }
+    }
+
+    private fun loadThis(resourceURL: String, root: String) {
+        resourceResolver.getResources("${root}/${resourceURL}/this.*")
+                .forEach {
+                    val mediaType = convertFileTypeToMediaType(it.filename!!)
+                    server.expect(requestTo(containsString(resourceURL))) // remove /stubs at start
+                            .andRespond(withSuccess()
+                                    .contentType(mediaType)
+                                    .body(it))
+                }
+    }
+
+    private fun convertFileTypeToMediaType(filename: String) =
+            if (filename.endsWith(".json")) MediaType.APPLICATION_JSON
+            else MediaType.ALL
+
     @Test
     fun getTvShow() {
-        val showId = "star-trek-tng"
-        server.expect(requestTo("https://api.trakt.tv/shows/$showId?extended=full,images"))
-                .andRespond(withSuccess()
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .body(objectMapper.writeValueAsString(Show())))
+        val showId = "star-trek-the-next-generation"
+//        val classPathResource = ClassPathResource("/stubs/shows/star-trek-the-next-generation/this.json")
+//        server.expect(requestTo("https://api.trakt.tv/shows/$showId?extended=full,images"))
+//                .andRespond(withSuccess()
+//                        .contentType(MediaType.APPLICATION_JSON)
+//                        .body(classPathResource))
         val show = traktMediaProvider.getShow(showId)
-
+        assertThat(show.title)
+                .isEqualTo("Star Trek: The Next Generation")
     }
 }
