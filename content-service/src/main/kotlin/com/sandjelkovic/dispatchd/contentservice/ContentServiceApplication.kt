25ba1b93f1afbcce73d0cc6c5f5af8be8b35d614
stefan <sa.stefan.andjelkovic@gmail.com>
2018-02-10 10:19:59 +0100

    Introduce daemon for content refresh

--- content-service/src/main/kotlin/com/sandjelkovic/dispatchd/contentservice/ContentServiceApplication.kt
+++ content-service/src/main/kotlin/com/sandjelkovic/dispatchd/contentservice/ContentServiceApplication.kt
@@ -2,13 +2,17 @@ package com.sandjelkovic.dispatchd.contentservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableAsync
+import org.springframework.scheduling.annotation.EnableScheduling
 
 /**
  * @author sandjelkovic
  * @date 14.1.18.
  */
-@SpringBootApplication
 
+@SpringBootApplication
+@EnableAsync
+@EnableScheduling
 class ContentServiceApplication
 
 fun main(args: Array<String>) {
