a1dbd8041c28249daea9cea95472ec8a74ee141f
stefan <sa.stefan.andjelkovic@gmail.com>
2018-03-18 10:20:08 +0100

    Add tests in Content service

--- content-service/src/test/kotlin/com/sandjelkovic/dispatchd/contentservice/trakt/provider/impl/DefaultTraktMediaProviderTest.kt
+++ content-service/src/test/kotlin/com/sandjelkovic/dispatchd/contentservice/trakt/provider/impl/DefaultTraktMediaProviderTest.kt
@@ -2,6 +2,7 @@ package com.sandjelkovic.dispatchd.contentservice.trakt.provider.impl
 
 import com.sandjelkovic.dispatchd.contentservice.trakt.dto.ShowTrakt
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.catchThrowable
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -50,5 +51,19 @@ class DefaultTraktMediaProviderTest {
         assertThat(tvShow)
                 .isEqualTo(preparedShow)
         verify(mockRestTemplate).getForObject<ShowTrakt>(uri)
+        verify(mockUriProvider).getShowUri(showId)
+    }
+
+    @Test
+    fun getTvShowInvalid() {
+        `when`(mockRestTemplate.getForObject<ShowTrakt>(uri))
+                .thenReturn(null)
+
+        val throwable = catchThrowable { provider.getShow(showId) }
+
+        assertThat(throwable).isInstanceOf(kotlin.NullPointerException::class.java)
+
+        verify(mockRestTemplate).getForObject<ShowTrakt>(uri)
+        verify(mockUriProvider).getShowUri(showId)
     }
 }
