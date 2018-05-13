e7114589ea45fce2f38309a0b8b63df09af51f02
stefan <sa.stefan.andjelkovic@gmail.com>
2018-05-13 17:59:38 +0200

    Split Configuration classes

--- content-service/src/main/kotlin/com/sandjelkovic/dispatchd/contentservice/ContentConfig.kt
+++ content-service/src/main/kotlin/com/sandjelkovic/dispatchd/contentservice/ContentConfig.kt
@@ -1,21 +1,16 @@
 package com.sandjelkovic.dispatchd.contentservice
 
-import com.sandjelkovic.dispatchd.contentservice.converter.Trakt2EpisodeConverter
-import com.sandjelkovic.dispatchd.contentservice.converter.Trakt2SeasonConverter
-import com.sandjelkovic.dispatchd.contentservice.converter.Trakt2ShowConverter
-import com.sandjelkovic.dispatchd.contentservice.data.repository.*
+import com.sandjelkovic.dispatchd.contentservice.data.repository.ImportStatusRepository
+import com.sandjelkovic.dispatchd.contentservice.data.repository.UpdateJobRepository
 import com.sandjelkovic.dispatchd.contentservice.interceptor.HeaderRequestInterceptor
 import com.sandjelkovic.dispatchd.contentservice.service.impl.DefaultContentRefreshService
 import com.sandjelkovic.dispatchd.contentservice.service.impl.DefaultImportService
-import com.sandjelkovic.dispatchd.contentservice.service.impl.DefaultTraktImporter
 import com.sandjelkovic.dispatchd.contentservice.trakt.provider.TraktMediaProvider
 import mu.KLogging
-import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.cloud.context.config.annotation.RefreshScope
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.core.convert.ConversionService
 import org.springframework.scheduling.annotation.EnableAsync
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
 import org.springframework.web.client.RestTemplate
@@ -31,11 +26,7 @@ import java.util.concurrent.Executor
 @EnableAsync
 class ContentConfig(
         @Value("\${content.refresh.interval.minutes:1}")
-        var refreshInterval: Int,
-        @Value("\${trakt.baseServiceUrl: }")
-        var baseServiceUrl: String,
-        @Value("\${trakt.appSecret: }")
-        var appSecret: String) {
+        var refreshInterval: Int) {
 
     companion object : KLogging()
 
@@ -57,20 +48,6 @@ class ContentConfig(
 
     @Bean
     fun importService(importStatusRepository: ImportStatusRepository) = DefaultImportService(importStatusRepository)
-
-    @Bean
-    fun traktImporter(showRepository: ShowRepository, seasonRepository: SeasonRepository,
-                      episodeRepository: EpisodeRepository, @Qualifier("mvcConversionService") conversionService: ConversionService, provider: TraktMediaProvider) =
-            DefaultTraktImporter(showRepository, seasonRepository, episodeRepository, conversionService, provider)
-
-    @Bean
-    fun trakt2EpisodeConverter() = Trakt2EpisodeConverter()
-
-    @Bean
-    fun trakt2SeasonConverter() = Trakt2SeasonConverter()
-
-    @Bean
-    fun trakt2ShowConverter() = Trakt2ShowConverter()
 
     @Bean(name = arrayOf("threadPoolTaskExecutor"))
     fun threadPoolTaskExecutor(): Executor {
---
+++ content-service/src/main/kotlin/com/sandjelkovic/dispatchd/contentservice/TraktConfig.kt
@@ -0,0 +1,46 @@
+package com.sandjelkovic.dispatchd.contentservice
+
+import com.sandjelkovic.dispatchd.contentservice.converter.Trakt2EpisodeConverter
+import com.sandjelkovic.dispatchd.contentservice.converter.Trakt2SeasonConverter
+import com.sandjelkovic.dispatchd.contentservice.converter.Trakt2ShowConverter
+import com.sandjelkovic.dispatchd.contentservice.data.repository.EpisodeRepository
+import com.sandjelkovic.dispatchd.contentservice.data.repository.SeasonRepository
+import com.sandjelkovic.dispatchd.contentservice.data.repository.ShowRepository
+import com.sandjelkovic.dispatchd.contentservice.service.impl.DefaultTraktImporter
+import com.sandjelkovic.dispatchd.contentservice.trakt.provider.TraktMediaProvider
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.cloud.context.config.annotation.RefreshScope
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.convert.ConversionService
+import org.springframework.scheduling.annotation.EnableAsync
+
+/**
+ * @author sandjelkovic
+ * @date 5.5.18.
+ */
+@Configuration
+@RefreshScope
+@EnableAsync
+class TraktConfig(
+        @Value("\${trakt.baseServiceUrl: }")
+        var baseServiceUrl: String,
+        @Value("\${trakt.appSecret: }")
+        var appSecret: String) {
+
+    @Bean
+    fun traktImporter(showRepository: ShowRepository, seasonRepository: SeasonRepository,
+                      episodeRepository: EpisodeRepository, @Qualifier("mvcConversionService") conversionService: ConversionService, provider: TraktMediaProvider) =
+            DefaultTraktImporter(showRepository, seasonRepository, episodeRepository, conversionService, provider)
+
+    @Bean
+    fun trakt2EpisodeConverter() = Trakt2EpisodeConverter()
+
+    @Bean
+    fun trakt2SeasonConverter() = Trakt2SeasonConverter()
+
+    @Bean
+    fun trakt2ShowConverter() = Trakt2ShowConverter()
+
+}
