ff13c31c9c52195301912cd9746fd50869d0d7f4
stefan <sa.stefan.andjelkovic@gmail.com>
2018-05-22 19:52:50 +0200

    Fix tests

--- core-service/src/main/java/com/sandjelkovic/dispatchd/configuration/EmailConfiguration.java
+++ core-service/src/main/java/com/sandjelkovic/dispatchd/configuration/EmailConfiguration.java
@@ -1,14 +1,13 @@
 package com.sandjelkovic.dispatchd.configuration;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @PropertySource({"classpath:/config/application-mail.properties", "classpath:/config/application-mailcredentials.properties"})
 public class EmailConfiguration {
-	@Value("${spring.mail.username}")
-	private String sourceEmail;
+	//	@Value("${spring.mail.username}")
+	private String sourceEmail = "";
 
 	public String getSourceEmail() {
 		return sourceEmail;
