223759e97611d0dfe0a510df4718ae482f652f87
stefan <sa.stefan.andjelkovic@gmail.com>
2018-08-02 15:18:31 +0200

    Implement Scheduled updates of content

--- content-service/src/main/kotlin/com/sandjelkovic/dispatchd/contentservice/service/ContentRefreshService.kt
+++ content-service/src/main/kotlin/com/sandjelkovic/dispatchd/contentservice/service/ContentRefreshService.kt
@@ -1,10 +1,12 @@
 package com.sandjelkovic.dispatchd.contentservice.service
 
+import arrow.core.Either
+
 /**
  * @author sandjelkovic
  * @date 10.2.18.
  */
 interface ContentRefreshService {
     fun updateAllContent(): Long
-    fun updateContentIfNeeded(): Long
+    fun updateContentIfNeeded(): Either<RemoteServiceException, Int>
 }
--- content-service/src/main/kotlin/com/sandjelkovic/dispatchd/contentservice/service/impl/DefaultContentRefreshService.kt
+++ content-service/src/main/kotlin/com/sandjelkovic/dispatchd/contentservice/service/impl/DefaultContentRefreshService.kt
@@ -1,86 +1,77 @@
 package com.sandjelkovic.dispatchd.contentservice.service.impl
 
+import arrow.core.Either
+import arrow.core.Option
+import arrow.core.Try
+import arrow.core.toOption
+import com.sandjelkovic.dispatchd.contentservice.data.entity.UpdateJob
+import com.sandjelkovic.dispatchd.contentservice.data.repository.ShowRepository
 import com.sandjelkovic.dispatchd.contentservice.data.repository.UpdateJobRepository
 import com.sandjelkovic.dispatchd.contentservice.service.ContentRefreshService
+import com.sandjelkovic.dispatchd.contentservice.service.RemoteServiceException
+import com.sandjelkovic.dispatchd.contentservice.service.ShowImporter
+import com.sandjelkovic.dispatchd.contentservice.trakt.dto.ShowUpdateTrakt
 import com.sandjelkovic.dispatchd.contentservice.trakt.provider.TraktMediaProvider
 import mu.KLogging
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.ZonedDateTime
 
 /**
  * @author sandjelkovic
  * @date 10.2.18.
  */
-class DefaultContentRefreshService(val updateJobRepository: UpdateJobRepository, val provider: TraktMediaProvider) : ContentRefreshService {
-    override fun updateContentIfNeeded(): Long {
-        return 0
+class DefaultContentRefreshService(val updateJobRepository: UpdateJobRepository, val provider: TraktMediaProvider, val showRepository: ShowRepository, val importer: ShowImporter) : ContentRefreshService {
+    override fun updateContentIfNeeded(): Either<RemoteServiceException, Int> {
+        return refreshExistingContent()
     }
 
     override fun updateAllContent(): Long {
-        return 0
+        TODO("Not yet implemented")
     }
 
     companion object : KLogging()
 
-    //    private val asyncResultMapper: java.util.function.Function<AsyncResult<TvShow>, TvShow>
-//        get() = { tvShowAsyncResult ->
-//            try {
-//                return tvShowAsyncResult.get(1, TimeUnit.MINUTES)
-//            } catch (ignored: ExecutionException) {
-//                return null
-//            }
-//        }
-//
-//
-//    private fun lastUpdateTime() {
-//        updateJobRepository.findFirstBySuccessOrderByFinishTimeDesc(true)
-//                .map(UpdateJob::finishTime)
-//                .orElseGet { ZonedDateTime.from(LocalDateTime.MIN, ZoneId.systemDefault()) }
-//    }
-//
-//    fun refreshExistingContent(): Long {
-//        val fromTime = lastUpdateTime()
-//        logger.debug("Refreshing content. Last update was: " + fromTime)
-//        val updatedShows = provider.getUpdates(fromTime.toLocalDate())
-//
-//        val traktIds = getTraktIds(updatedShows)
-//        val idsForImport = filterForLocalIds(traktIds)
-//        log.debug("Shows to be imported (Trakt IDs): " + traktIds.toString())
-//
+    fun refreshExistingContent(): Either<RemoteServiceException, Int> =
+            getLastUpdateTime().also {
+                logger.debug("Refreshing content. Last update was: $it")
+            }.let { fromTime ->
+                provider.getUpdates(fromTime.toLocalDate())
+                        .map { getIdsForUpdate(it) }
+                        .map { it.also { logger.debug { "Shows to be imported (Trakt IDs): $it" } } }
+                        .map { ids -> ids.map { Try { importer.importShow(it) } }.count { it.isSuccess() } }
+                        .also { createNewJobReport() }
+            }
 //        // possible optimisation for failure cases -> scan internal db and compare retrieved.updatedAt < internal.lastLocalUpdate
 //        // in order to only update shows that failed in  the past. Since the update time is started from the last successful refresh.
-//
-//        // independent update of each show in order to continue the refresh evens if some fail. Otherwise, this could have been one stream. (hint!, hint!)
-//        val importResults = idsForImport.stream()
-//                .map<AsyncResult<TvShow>> { id -> traktImporterService!!.importShowAsync(id) }
-//                .collect<List<AsyncResult<TvShow>>, Any>(toList<AsyncResult<TvShow>>())
-//
-//        val count = importResults.stream()
-//                .map<TvShow>(asyncResultMapper)
-//                .filter(Predicate<TvShow> { Objects.nonNull(it) })
-//                .count()
-//
-//        saveJob()
-//        return count
-//    }
-//
-//    private fun filterForLocalIds(traktIds: List<String>): List<String> {
-//        return traktIds.stream()
-//                .filter { id -> tvShowService!!.findByTraktId(id).isPresent() }
-//                .collect<List<String>, Any>(toList<String>())
-//    }
-//
-//    private fun getTraktIds(updatedShows: List<ShowUpdateTrakt>): List<String> {
-//        return updatedShows.stream()
-//                .map<TvShowTrakt>(Function<ShowUpdateTrakt, TvShowTrakt> { it.getShow() })
-//                .map<Map<String, String>>(Function<TvShowTrakt, Map<String, String>> { it.getIds() })
-//                .filter { idMap -> idMap.containsKey("trakt") }
-//                .map<String> { idMap -> idMap["trakt"] }
-//                .collect<List<String>, Any>(toList<String>())
-//    }
-//
-//    private fun saveJob() {
-//        val job = jobRepository!!.save(UpdateJob()
-//                .finishTime(ZonedDateTime.now())
-//                .success(true))
-//        log.debug("Saved job: " + job)
-//    }
+//        // independent update of each show in order to continue the refresh evens if some fail.
+
+    private fun getIdsForUpdate(it: List<ShowUpdateTrakt>): List<String> {
+        return it.map { extractTraktId(it) }
+                .mapNotNull { it.orNull() }
+                .filter { id -> showExists(id) }
+    }
+
+    private fun showExists(id: String) = showRepository.findByTraktId(id).isPresent
+
+    private fun extractTraktId(update: ShowUpdateTrakt): Option<String> = update.toOption()
+            .flatMap { it.show.toOption() }
+            .map { it.ids }
+            .filter { it.containsKey("trakt") }
+            .map { it["trakt"]!! }
+
+    private fun getLastUpdateTime(): ZonedDateTime =
+            updateJobRepository.findFirstBySuccessOrderByFinishTimeDesc(true)
+                    .map { it.finishTime }
+                    .orElseGet { ZonedDateTime.of(LocalDateTime.MIN, ZoneId.systemDefault()) }
+
+    private fun createNewJobReport() =
+            updateJobRepository.save(
+                    UpdateJob().apply {
+                        finishTime = ZonedDateTime.now()
+                        success = true
+                    }
+            ).also {
+                logger.debug("Saved job: $it")
+            }
 }
