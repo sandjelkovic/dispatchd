8a2e296ffd00eb2c2269f4e7c3cdc8c8719a0827
stefan <sa.stefan.andjelkovic@gmail.com>
2018-08-02 14:12:21 +0200

    Extend TraktMedia Provider functionality

--- content-service/src/main/kotlin/com/sandjelkovic/dispatchd/contentservice/service/RemoteServiceException.kt
+++ content-service/src/main/kotlin/com/sandjelkovic/dispatchd/contentservice/service/RemoteServiceException.kt
@@ -1,9 +1,7 @@
 package com.sandjelkovic.dispatchd.contentservice.service
 
-import org.springframework.web.client.HttpClientErrorException
-
 /**
  * @author sandjelkovic
  * @date 24.3.18.
  */
-class RemoteServiceException(e: HttpClientErrorException) : RuntimeException()
+class RemoteServiceException(e: Throwable) : RuntimeException()
--- content-service/src/main/kotlin/com/sandjelkovic/dispatchd/contentservice/trakt/provider/TraktMediaProvider.kt
+++ content-service/src/main/kotlin/com/sandjelkovic/dispatchd/contentservice/trakt/provider/TraktMediaProvider.kt
@@ -1,10 +1,14 @@
 package com.sandjelkovic.dispatchd.contentservice.trakt.provider
 
+import arrow.core.Either
+import com.sandjelkovic.dispatchd.contentservice.service.RemoteServiceException
 import com.sandjelkovic.dispatchd.contentservice.trakt.dto.EpisodeTrakt
 import com.sandjelkovic.dispatchd.contentservice.trakt.dto.SeasonTrakt
 import com.sandjelkovic.dispatchd.contentservice.trakt.dto.ShowTrakt
+import com.sandjelkovic.dispatchd.contentservice.trakt.dto.ShowUpdateTrakt
 import org.springframework.scheduling.annotation.Async
 import org.springframework.scheduling.annotation.AsyncResult
+import java.time.LocalDate
 import java.util.*
 
 /**
@@ -17,6 +21,7 @@ interface TraktMediaProvider {
     fun getSeasons(showId: String): List<SeasonTrakt>
     fun getSeasonsMinimal(showId: String): List<SeasonTrakt>
     fun getSeasonEpisodes(showId: String, seasonNumber: String): List<EpisodeTrakt>
+    fun getUpdates(fromDate: LocalDate): Either<RemoteServiceException, List<ShowUpdateTrakt>>
 
     @Async
     fun getSeasonsAsync(showId: String): AsyncResult<List<SeasonTrakt>>
