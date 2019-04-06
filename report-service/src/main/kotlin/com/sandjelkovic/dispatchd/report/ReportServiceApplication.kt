ef9c852eea622cb8961ac0cefbf9ac5c7b274d6f
StefanAndjelkovic <sa.stefan.andjelkovic@gmail.com>
2019-04-06 16:21:00 +0200

    #37 Generate new Spring project

---
+++ report-service/src/main/kotlin/com/sandjelkovic/dispatchd/report/ReportServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.sandjelkovic.dispatchd.report
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ReportServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<ReportServiceApplication>(*args)
+}
