54c6f95452847fd4210bb43a19848c1ae5affcce
StefanAndjelkovic <sa.stefan.andjelkovic@gmail.com>
2019-05-03 11:30:22 +0200

    #41 Create sample routes in the Gateway

--- gateway-service/src/main/kotlin/com/sandjelkovic/dispatchd/gateway/Gateway.kt
+++ gateway-service/src/main/kotlin/com/sandjelkovic/dispatchd/gateway/Gateway.kt
@@ -2,9 +2,27 @@ package com.sandjelkovic.dispatchd.gateway
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.gateway.route.RouteLocator
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class Gateway
+class Gateway {
+    @Bean
+    fun customRouteLocator(builder: RouteLocatorBuilder): RouteLocator = builder
+        .routes()
+        .route("monitor-service") { spec ->
+            spec.path("/monitor-service/**")
+                .filters { it.stripPrefix(1) }
+                .uri("lb://monitor-service")
+        }
+        .route("content-service") { spec ->
+            spec.path("/content-service/**")
+                .filters { it.stripPrefix(1) }
+                .uri("lb://content-service")
+        }
+        .build()
+}
 
 fun main(args: Array<String>) {
     runApplication<Gateway>(*args)
